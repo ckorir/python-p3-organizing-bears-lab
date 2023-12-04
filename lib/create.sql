a1835b60cec7328a9bdd009f5aa7fd8a4c6349fe
ckorir <chrisruto.k@gmail.com>
2023-12-04 11:20:41 +0300

    Completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS bears (id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT, age INTEGER, sex TEXT, color TEXT, temperament TEXT, alive Boolean);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES('Mr. Chocolate',23,'M','brown','intense',False),
+('Rowdy',24,'M','black','aggresive',TRUE),
+('Tabitha',25,'F','red','clingy',False),
+('Sergeant Brown',26,'M','blue','calm',TRUE),
+('Melissa',27,'F','white','naive',False),
+('Grinch',28,'M','orange','goofy',TRUE),
+('Wendy',29,'F','pink','nice',False),
+(NULL,30,'M','green','slimy',False);
